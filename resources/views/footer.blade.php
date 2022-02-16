9194ec9e8d6b3a179b70c1ba880757c89a65e3b3
hightt <konrad.duda121@gmail.com>
2022-02-16 17:30:34 +0100

    Footer template

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,10 +1,38 @@
 <footer class="main-footer footer mt-auto py-3 bg-light">
-    <div class="container-fluid">
+    <div class="container">
      <div class="row">
          <div class="col-6">
-
+             <div class="content me-2">
+                <span class="fw-bold">Polityka prywatności</span>
+                <p class="text-muted">In pharetra ultricies consectetur. Vivamus ut magna posuere,
+                    malesuada nunc in, accumsan nisl. Ut congue eget mauris sed luctus.
+                    Sed ut mollis ex, id feugiat est. Nullam sed auctor leo. Phasellus pellentesque,
+                    nisi ac lobortis ullamcorper, massa dolor tempor augue, molestie imperdiet mauris mi vitae turpis.
+                    Fusce id metus tincidunt, pellentesque nibh id, ornare justo.
+                    Cras quis odio nunc. Sed erat metus, sollicitudin non maximus eget, cursus a velit.
+                </p>
+            </div>
          </div>
          <div class="col-6">
+             <div class="content ms-2">
+                <span class="fw-bold">Kontakt</span>
+                <p class="text-muted m-0">
+                    <i class="fa-solid fa-envelope me-1"></i> konrad.duda121@gmail.com
+                </p>
+                <p class="text-muted m-0">
+                    <i class="fa-solid fa-phone me-1"></i> +48 123 456 789
+                </p>
+                <p class="text-muted m-0">
+                    <i class="fa-brands fa-facebook me-1"></i>
+                    <a href="https://www.facebook.com/konrad.duda.167"> Konrad Duda </a>
+                </p>
+                <p class="text-muted m-0">
+                    <i class="fa-brands fa-github me-1"></i>
+                    <a href="https://github.com/hightt"> hightt </a>
+                </p>
+
+             </div>
+
 
          </div>
      </div>
