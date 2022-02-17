5a804aef6b86b6f9e203e07d8b30b73a2c7f2c48
hightt <konrad.duda121@gmail.com>
2022-02-17 12:08:38 +0100

    Change icon

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,19 +1,19 @@
 
-<div class="content me-3">
+<div class="content">
     <nav class="left-nav nav flex-column">
         <a class="nav-link mb-2" href="{{asset("/posts")}}">
             <i class="fas fa-home me-2"></i>
             <span>Strona główna</span>
         </a>
         <a class="nav-link mb-2" href="#">
-            <i class="fas fa-eye me-2"></i>
-            <span>Ostatnio odwiedzane</span>
+            <i class="fa-solid fa-star me-2" style="color: #FFEA00;"></i>
+            <span>Ulubione</span>
         </a>
         <a class="nav-link mb-2" href="{{asset('my-topics')}}">
             <i class="fas fa-plus-square me-2"></i>
             <span>Moje tematy</span>
         </a>
-        <a class="nav-link border-0" href="#">
+        <a class="nav-link border-0" href="{{asset('my-answers')}}">
             <i class="fas fa-question me-2"></i>
             <span>Moje odpowiedzi</span>
         </a>
