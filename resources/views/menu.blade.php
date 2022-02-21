22a504447bec9223758246f6dd00bee8f7304eba
hightt <konrad.duda121@gmail.com>
2022-02-21 21:02:01 +0100

    Links are not available if not logged

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -9,11 +9,11 @@
             <i class="fa-solid fa-star me-2" style="color: #FFEA00;"></i>
             <span>Ulubione</span>
         </a>
-        <a class="nav-link mb-2" href="{{asset('my-topics')}}">
+        <a class="nav-link mb-2 {{Auth::check() ? '' : 'disabled'}}" href="{{asset('my-topics')}}">
             <i class="fas fa-plus-square me-2"></i>
             <span>Moje tematy</span>
         </a>
-        <a class="nav-link border-0" href="{{asset('my-answers')}}">
+        <a class="nav-link border-0 {{Auth::check() ? '' : 'disabled'}}" href="{{asset('my-answers')}}">
             <i class="fas fa-question me-2"></i>
             <span>Moje odpowiedzi</span>
         </a>
