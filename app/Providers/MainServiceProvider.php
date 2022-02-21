1c985cb0071633b87434d53702aeced3c029178e
hightt <konrad.duda121@gmail.com>
2022-02-21 20:59:02 +0100

    New function theMostAcriveUsers()

--- app/Providers/MainServiceProvider.php
+++ app/Providers/MainServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\User;
-
+use App\Models\Post;
 class MainServiceProvider extends ServiceProvider
 {
     /**
@@ -25,14 +25,22 @@ class MainServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $best_users = collect(User::all())->map(function($item) {
+
+        View::share('best_users', $this->theMostActiveUsers());
+
+        View::composer('index', function($view) {
+            $view->with('posts', Post::orderBy('created_at', 'DESC')->paginate(10));
+        });
+    }
+
+    public function theMostActiveUsers()
+    {
+        return collect(User::all())->map(function($item) {
             return [
                     'id' => $item->id,
                     'num' => User::find($item->id)->comments()->count(),
                     'user_name' => User::where('id', $item->id)->value("name")
                 ];
         })->sortByDesc('num')->take(8)->values()->toArray();
-
-        View::share('best_users', $best_users);
     }
 }
