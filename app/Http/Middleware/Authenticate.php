92fe29c4bca82b3463fcb103de4abf2508937077
hightt <konrad.duda121@gmail.com>
2022-02-21 20:58:15 +0100

    Change redirect after not authenticated to route('posts.index')

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -10,12 +10,12 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+    //  * @return string|null
      */
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return route('posts.index');
         }
     }
 }
