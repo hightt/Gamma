6039b9ec32b5053a1189fc292d3e8c3f0a978e77
hightt <konrad.duda121@gmail.com>
2022-02-22 13:19:02 +0100

    CSS changes

--- resources/views/modals/new_post_modal.blade.php
+++ resources/views/modals/new_post_modal.blade.php
@@ -2,24 +2,23 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Dodaj nowy post</h5>
+          <h5 class="modal-title">Dodaj nowy post</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <form action="{{route('posts.store')}}" method="POST">
             @csrf
             <div class="modal-body">
                 <div class="mb-3">
-                    <label for="title" class="form-text">Tytuł posta</label>
-                    <input type="text" id="title" name="title" class="form-control @error('title') is-invalid @enderror" value="{{old('title')}}">
-
-                    @error('title')
+                    <label for="post_title" class="form-text">Tytuł posta</label>
+                    <input type="text" id="post_title" name="post_title" class="form-control @error('post_title') is-invalid @enderror" value="{{old('post_title')}}">
+                    @error('post_title')
                         <div class="invalid-feedback d-block">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
-                    <label for="title" class="form-text">Zawartość</label>
-                    <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="4" style="resize: none;"></textarea>
-                    @error('content')
+                    <label for="post_content" class="form-text">Zawartość</label>
+                    <textarea class="form-control @error('post_content') is-invalid @enderror"  id="post_content" name="post_content" rows="4" style="resize: none;"></textarea>
+                    @error('post_content')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                 @enderror
                 </div>
@@ -31,11 +30,12 @@
         </form>
       </div>
     </div>
-    @if ($errors->any() && Route::currentRouteName() == 'posts.index')
+    {{-- @if($errors->has('title') || $errors->has(''))
+    {{dd($errors)}}
         <script type="text/javascript">
             $(window).on('load',function(){
                 $('#add_post').modal('show');
             });
         </script>
-    @endif
+    @endif --}}
 </div>
