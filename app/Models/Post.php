71d78377d41286b5598a698f32f2233035886b0f
hightt <konrad.duda121@gmail.com>
2022-02-22 13:12:15 +0100

    add scopeMyComments()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class Post extends Model
 {
@@ -27,6 +29,16 @@ class Post extends Model
     {
         // return $this->created_at->format('Y-m-d');
         // return Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)->format('Y-m-d');
+    }
+
+    public function userCommentsBelongsToPost()
+    {
+        return $this->comments()->where('user_id', Auth::user()->id);
+    }
+
+    public function scopeMycomments($query)
+    {
+        return $query->where('user_id', Auth::user()->id);
     }
 
 }
