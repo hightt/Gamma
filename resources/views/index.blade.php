e496e71c6d85a1136598faf8de6d23fbd1fc2341
hightt <konrad.duda121@gmail.com>
2022-02-22 16:09:17 +0100

    Div post - change bootstrap grid to add new button (fav-btn)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,26 +4,28 @@
         <h3 class="mt-5 text-center">Brak dostępnych postów</h3>
     @else
         @foreach($posts as $post)
-            <div class="post">
+            <div class="post" style="padding-top: 15px !important;">
                 <div class="row">
-                    @if(Auth::check() && Auth::user()->permissions == '1')
-                        <div class="col-md-12 text-end">
-                            <div class="dropdown dropdown-options">
-                                <button class="user-options post-options" type="button" data-bs-toggle="dropdown">
-                                    <i class="fa-solid fa-ellipsis-vertical" style="font-size: 20px;" ></i>
+                    <div class="col-md-12 text-end">
+                        <form action="{{route('posts.destroy', $post->id)}}" method="POST" class="d-inline-block">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="fav-btn btn bg-warning text-white p-1 ps-2 pe-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Dodaj do ulubionych">
+                                <i class="fa-solid fa-star "></i>
+                            </button>
+                        </form>
+                        @if(Auth::check() && Auth::user()->permissions == '1')
+                            <form action="{{route('posts.destroy', $post->id)}}" method="POST" class="d-inline-block ">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="fav-btn btn bg-danger text-white p-1 ps-2 pe-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Usuń post">
+                                    <i class="fa-solid fa-trash"></i>
                                 </button>
-                                <ul class="dropdown-menu">
-                                    <form action="{{route('posts.destroy', $post->id)}}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                       <input type="submit" value="Usuń post" class="dropdown-item">
-                                    </form>
-                                </ul>
-
-                            </div>
-                        </div>
-                    @endif
+                            </form>
+                        @endif
+                    </div>
                 </div>
+
                 <a href="{{route('posts.show', $post->id)}}">
                     <div class="col-lg-12">
                         <div class="row">
