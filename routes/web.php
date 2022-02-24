a708d24981f0db751fb77f717826634d9bb262de
hightt <konrad.duda121@gmail.com>
2022-02-24 10:34:48 +0100

    New Route::resource ajax-call

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\CommentsController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\FavouritePostsController;
+use App\Http\Controllers\PostsAjaxController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -20,9 +23,16 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 Route::resource('/posts', PostsController::class);
 Route::resource('/comments', CommentsController::class);
-Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Route::get('/logout', [LoginController::class, 'logout']);
 Route::get('/search', [PostsController::class, 'search']);
 Route::get('/my-topics', [PostsController::class, 'myTopics'])->middleware('auth');
 Route::get('/my-answers', [PostsController::class, 'myAnswers'])->middleware('auth');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::resource('/favourite-post', FavouritePostsController::class)->except([
+    'create', 'update', 'show', 'edit', 'destroy'
+]);
+
+Route::delete('/delete-fav', [FavouritePostsController::class, 'delete'])->name('favourite-posts.delete');
+
+Route::delete('/posts-ajax', [PostsAjaxController::class, 'destroy'])->name('posts-ajax.destroy');
+Route::resource('/posts-ajax', PostsAjaxController::class)->except('create', 'update', 'edit', 'show', 'destroy');
