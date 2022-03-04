1842494f74deb9c3a874e328ae5e7d670bf6ae3c
hightt <konrad.duda121@gmail.com>
2022-03-04 13:36:21 +0100

    Improvement href links

--- resources/views/sections/menu.blade.php
+++ resources/views/sections/menu.blade.php
@@ -5,15 +5,15 @@
             <i class="fas fa-home me-2"></i>
             <span>Strona główna</span>
         </a>
-        <a class="nav-link mb-2 main-link" href="{{route('favourite-post.index')}}" data-bs-toggle="tooltip" data-bs-placement="left" title="">
+        <a class="nav-link mb-2 main-link {{Auth::check() ? '' : 'disabled-link'}}" href="{{Auth::check() ? route('favourite-post.index') : 'javascript: return false;'}}" data-bs-toggle="tooltip" data-bs-placement="left" title="">
             <i class="fa-solid fa-star me-2"></i>
             <span>Ulubione</span>
         </a>
-        <a class="nav-link mb-2 main-link {{Auth::check() ? '' : 'disabled-link'}}" href="{{asset('my-topics')}}" data-bs-toggle="tooltip" data-bs-placement="left" title="">
+        <a class="nav-link mb-2 main-link {{Auth::check() ? '' : 'disabled-link'}}" href="{{Auth::check() ? route('my-topics') : 'javascript: return false;'}}" data-bs-toggle="tooltip" data-bs-placement="left" title="">
             <i class="fas fa-plus-square me-2"></i>
             <span>Moje tematy</span>
         </a>
-        <a class="nav-link border-0 main-link {{Auth::check() ? '' : 'disabled-link'}}" href="{{asset('my-answers')}}" data-bs-toggle="tooltip" data-bs-placement="left" title="">
+        <a class="nav-link border-0 main-link {{Auth::check() ? '' : 'disabled-link'}}" href="{{Auth::check() ? route('my-answers') : 'javascript: return false;'}}" data-bs-toggle="tooltip" data-bs-placement="left" title="">
             <i class="fas fa-question me-2"></i>
             <span>Moje odpowiedzi</span>
         </a>
@@ -24,8 +24,7 @@
     $('.main-link').each(function(index,item){
         var classStr = $(this).attr('class');
         if(classStr.substr(classStr.lastIndexOf(' ') + 1) == 'disabled-link') {
-            $(this).attr('data-bs-original-title', 'Musisz być zalogowany, aby przejść dalej.')
+            $(this).attr('data-bs-original-title', 'Musisz być zalogowany, aby przejść dalej')
         }
-
     });
 </script>
